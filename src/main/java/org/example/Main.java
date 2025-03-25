7eedfef85e1e5cf4614676df5bbb4756ba7b1cc8
jashhpatel <jashhpatel25@gmail.com>
2025-03-25 15:46:13 +0530

    Added Area prog

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome! Lets calculate area of triangle");
+        System.out.println("Enter the Length of base");
+        int base = ip.nextInt();
+        System.out.println("Enter the length of altitude/height");
+        int height = ip.nextInt();
+        int area = (int) (0.5*base*height);
+        System.out.println("The area of triangle is "+ area);
+    }
+}
